e5ac142ea3fabe6d855daedd0c2ef779a01e1ea9
Death <temptationx@me.com>
2015-03-15 17:29:59 +0800

    sq c api

---
+++ src/sq_ext.cpp
@@ -0,0 +1,58 @@
+#include "one.hpp"
+Sq *sq = NULL;
+
+#define SQ_API __declspec(dllexport)
+
+EXTERN_C SQ_API void init(const char* dir, const char *log_filename, int inter, int proxy_port, int log_enable)
+{
+	sq = new Sq(dir, inter, proxy_port);
+	if (log_enable) {
+		sq->enable_log(log_filename);
+	}
+}
+
+EXTERN_C SQ_API void start()
+{
+	sq->start();
+}
+
+EXTERN_C SQ_API void stop()
+{
+	sq->stop();
+}
+
+EXTERN_C SQ_API void add_rule(const char *path, const char *url_rule, const char *body_rule)
+{
+	sq->add_rule(path, url_rule, body_rule);
+}
+
+BOOL WINAPI DllMain(HINSTANCE module_handle, DWORD reason_for_call, LPVOID reserved)
+{
+	if (reason_for_call == DLL_PROCESS_ATTACH) // Self-explanatory
+	{
+		DisableThreadLibraryCalls(module_handle); // Disable DllMain calls for DLL_THREAD_*
+		//if (reserved == NULL) // Dynamic load
+		//{
+		//	//foobar_meow(); // Initialize your stuff or whatever
+		//	// Return FALSE if you don't want your module to be dynamically loaded
+		//}
+		//else // Static load
+		//{
+		//	// Return FALSE if you don't want your module to be statically loaded
+		//	return FALSE;
+		//}
+	}
+
+	if (reason_for_call == DLL_PROCESS_DETACH) // Self-explanatory
+	{
+		if (reserved == NULL) // Either loading the DLL has failed or FreeLibrary was called
+		{
+			// Cleanup
+		}
+		else // Process is terminating
+		{
+			// Cleanup
+		}
+	}
+	return TRUE;
+}
